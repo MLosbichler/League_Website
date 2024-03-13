01ba7090e0f8aa906743b8740c4a09d6b3a26f55
Manuel Losbichler <53006634+MLosbichler@users.noreply.github.com>
2024-03-13 08:57:14 +0100

    first commit

---
+++ Backend/league_project/src/main/java/bichla/league_project/LeagueProjectApplication.java
@@ -0,0 +1,13 @@
+package bichla.league_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueProjectApplication.class, args);
+	}
+
+}
