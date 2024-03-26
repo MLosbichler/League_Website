10b5a6488e204fd9089b4c51365b498f6d6b6622
Manuel Losbichler <53006634+MLosbichler@users.noreply.github.com>
2024-03-26 19:30:45 +0100

    Created the repositories to enable saving to and retrieving data from the database. In this step every logical way to search the database has been implemented, even though most won´t be accessed later.

---
+++ Backend/league_project/src/main/java/bichla/league_project/repository/LeagueEntryRepository.java
@@ -0,0 +1,28 @@
+package bichla.league_project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import bichla.league_project.model.entity.LeagueEntry;
+import java.util.Optional;
+import java.util.List;
+
+/**
+ * 
+ * 
+ * @author      Manuel Losbichler
+ * @version     %I%, %G%
+ */
+@Repository
+public interface LeagueEntryRepository extends JpaRepository<LeagueEntry, Long>{
+    @SuppressWarnings("null")
+    Optional<LeagueEntry> findById(Long id);
+    List<LeagueEntry> findBySummonerId(String summonerId);
+    List<LeagueEntry> findByLeagueId(String leagueId);
+    List<LeagueEntry> findBySummonerName(String summonerName);
+    List<LeagueEntry> findByQueueType(String queueType);
+    
+    @SuppressWarnings("null")
+    @Override
+    <S extends LeagueEntry> S save(S entity);
+}
---
+++ Backend/league_project/src/main/java/bichla/league_project/repository/SummonerRepository.java
@@ -0,0 +1,26 @@
+package bichla.league_project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import bichla.league_project.model.entity.Summoner;
+import java.util.List;
+
+/**
+ * 
+ * 
+ * @author      Manuel Losbichler
+ * @version     %I%, %G%
+ */
+@Repository
+public interface SummonerRepository extends JpaRepository<Summoner, Long> {
+    List<Summoner> findById(String id);
+    List<Summoner> findByAccountId(String accountId);
+    List<Summoner> findByPuuid(String puuid);
+    List<Summoner> findBySummonerId(Long summonerId);
+    List<Summoner> findByName(String name);
+
+    @SuppressWarnings("null")
+    @Override
+    <S extends Summoner> S save(S entity);
+}
